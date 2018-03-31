8cbf8ed4b1557c1b8c6cd6d9b5d7d4ce192d85a8
Jess <s3529550@student.rmit.edu.au>
2018-03-31 15:44:50 +1100

    implemented read every line

--- src/database_ass1/Main.java
+++ src/database_ass1/Main.java
@@ -1,9 +1,30 @@
 package database_ass1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try{
+			File file = new File("./src/database_ass1/test.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			StringBuffer sb = new StringBuffer();
+			String line = br.readLine();
+			while(line !=null){
+				sb.append(line);
+				line = br.readLine();
+			}
+			fr.close();
+			System.out.println(sb.toString());
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
 
 	}
 
