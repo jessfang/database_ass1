29da5afa663b203dd504427a1b550372bb9d776e
Jess <s3529550@student.rmit.edu.au>
2018-05-13 14:24:34 +1000

    Created a hash function to hash strings

--- src/HashTable.java
+++ src/HashTable.java
@@ -10,5 +10,16 @@ public class HashTable {
         for (int x = 0; x < length_table; x++) {
             hashtable[x] = null;
         }
-    } 
+    }
+    
+    /* A function to create a hash from a given string */
+    private int hash(String key)
+    {
+        int hashVal = key.hashCode();
+        hashVal = hashVal % length_table;
+        // in case hashVal is less than 0 which would cause an error add table length
+        if (hashVal < 0)
+            hashVal = hashVal + length_table;
+        return hashVal;
+    }
 }
